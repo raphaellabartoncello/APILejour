7e1580b40afab6785122d4c82cd2b1a87245c9af
Raphaella Karine Nascimento Bartoncello <rbartoncello@springglobal.com>
2020-11-18 18:51:38 -0300

    Add project files.

---
+++ APILejour/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace APILejour
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtURI = new System.Windows.Forms.TextBox();
+            this.gdvDados = new System.Windows.Forms.DataGridView();
+            this.btnObterProdutos = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gdvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URI - Web API";
+            // 
+            // txtURI
+            // 
+            this.txtURI.Location = new System.Drawing.Point(98, 5);
+            this.txtURI.Name = "txtURI";
+            this.txtURI.Size = new System.Drawing.Size(681, 20);
+            this.txtURI.TabIndex = 1;
+            // 
+            // gdvDados
+            // 
+            this.gdvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gdvDados.Location = new System.Drawing.Point(17, 89);
+            this.gdvDados.Name = "gdvDados";
+            this.gdvDados.Size = new System.Drawing.Size(762, 331);
+            this.gdvDados.TabIndex = 2;
+            // 
+            // btnObterProdutos
+            // 
+            this.btnObterProdutos.Location = new System.Drawing.Point(664, 31);
+            this.btnObterProdutos.Name = "btnObterProdutos";
+            this.btnObterProdutos.Size = new System.Drawing.Size(115, 41);
+            this.btnObterProdutos.TabIndex = 3;
+            this.btnObterProdutos.Text = "button1";
+            this.btnObterProdutos.UseVisualStyleBackColor = true;
+            this.btnObterProdutos.Click += new System.EventHandler(this.btnObterProdutos_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnObterProdutos);
+            this.Controls.Add(this.gdvDados);
+            this.Controls.Add(this.txtURI);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gdvDados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtURI;
+        private System.Windows.Forms.DataGridView gdvDados;
+        private System.Windows.Forms.Button btnObterProdutos;
+    }
+}
+
---
+++ APILejour/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace APILejour
+{
+    public partial class Form1 : Form
+    {
+            string URI = "";
+            int codigoProduto = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void GetAllProdutos()
+        {
+            URI = txtURI.Text;
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.GetAsync(URI))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var ProdutoJsonString = await response.Content.ReadAsStringAsync();
+                        gdvDados.DataSource = JsonConvert.DeserializeObject<User[]>(ProdutoJsonString).ToList();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Não foi possível obter o produto : " + response.StatusCode);
+                    }
+                }
+            }
+        }
+
+        private void btnObterProdutos_Click(object sender, EventArgs e)
+        {
+            GetAllProdutos();
+        }
+    }
+}
